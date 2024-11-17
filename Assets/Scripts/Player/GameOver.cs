35efa751b96edcab175478aaac428db719d8e355
T-465 <Dunne-T2@ulster.ac.uk>
2024-11-17 15:08:25 +0000

    click wont work

--- Assets/Scripts/Player/GameOver.cs
+++ Assets/Scripts/Player/GameOver.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
+using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
@@ -9,13 +11,22 @@ public class GameOver : MonoBehaviour
     public GameObject flashlight;
     public GameObject mothman;
 
-   public void OnGameOver()
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+    public void OnGameOver()
    {
         globalVol.SetActive(false);
         gameOver.SetActive(true);
         flashlight.SetActive(false);
         mothman.SetActive(false);
-   }
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
 
-
+  public void Retry()
+  {
+        SceneManager.LoadScene("Mothman");
+  }
 }
