fa887b28bae687fe796f7cc5b9754361ea4ad8b9
T-465 <Dunne-T2@ulster.ac.uk>
2024-11-30 22:01:34 +0000

    fixedupdate

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -21,7 +21,7 @@ public class Player : MonoBehaviour, IDamageable
         cc = GetComponent<CharacterController>();
 
     }
-    private void Update()
+    private void FixedUpdate()
     {
         #region PlayerMove
 
